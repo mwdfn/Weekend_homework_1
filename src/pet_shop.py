31e1482727c1ea6686a8d9372460e6a4f8b083e3
Mark Woodfine-Jones <mwdfnj@gmail.com>
2022-04-01 15:40:59 +0100

    Test 5 passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,9 +1,21 @@
 # WRITE YOUR FUNCTIONS HERE
 
-# Test 1 return pet shop name
+# Test 1 get pet shop name
 def get_pet_shop_name(pet_shop):
     return pet_shop ["name"]
 
-# Test 2 calculate total cash
+# Test 2 get total cash
 def get_total_cash(cash):
     return cash ["admin"]["total_cash"]
+
+# Test 3 & 4 add/remove cash
+def add_or_remove_cash(pet_shop_admin, new_total_cash):
+    pet_shop_admin["admin"]["total_cash"] += new_total_cash
+
+# Test 5 gets total pets sold
+def get_pets_sold(total_pets_sold):
+    return total_pets_sold["admin"]["pets_sold"]
+
+
+    
+
