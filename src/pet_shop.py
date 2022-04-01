cfb139dd452d4f9037055b7e3af08e968508aedc
Mark Woodfine-Jones <mwdfnj@gmail.com>
2022-04-01 14:56:22 +0100

    first commit

---
+++ src/pet_shop.py
@@ -0,0 +1,9 @@
+# WRITE YOUR FUNCTIONS HERE
+
+# Test 1 return pet shop name
+def get_pet_shop_name(pet_shop):
+    return pet_shop ["name"]
+
+# Test 2 calculate total cash
+def get_total_cash(cash):
+    return cash ["admin"]["total_cash"]
