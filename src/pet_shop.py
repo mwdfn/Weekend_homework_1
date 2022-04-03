e02f5ee49816ffb6248439125f536141802ff1d8
Mark Woodfine-Jones <mwdfnj@gmail.com>
2022-04-03 20:35:02 +0100

    Test 18 passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -65,6 +65,14 @@ def get_customer_pet_count(customer):
 def add_pet_to_customer(customer, new_pet):
     customer["pets"].append(new_pet)
 
+# --OPTIONAL--
+
+def customer_can_afford_pet(customer, new_pet):
+    if customer["cash"] >= new_pet["price"]:
+        return True
+    else:
+        return False
+
 
     
             
